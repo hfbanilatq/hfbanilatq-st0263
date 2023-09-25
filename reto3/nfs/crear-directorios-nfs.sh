a0d4ba8560dff0ae4d17b79edabe4ebe5802467d
Hector Fabio Banilat Quintero <53194376+hfbanilatq@users.noreply.github.com>
2023-09-25 10:03:29 -0500

    intentando solucion para nfs

--- reto3/nfs/crear-directorios-nfs.sh
+++ reto3/nfs/crear-directorios-nfs.sh
@@ -1,5 +1,5 @@
-sudo mkdir -p /nfsshared
+sudo mkdir -p /var/www/html
 sudo emacs -nw /etc/exports #Abre el archivo usando emacs, puedes usar nano o el lector de archivos y debes agregar lo siguiente:
-#/nfsshared      *(rw,sync,wdelay,hide,no_subtree_check,sec=sys,insecure,no_root_squash,no_all_squash) esto permite el acceso de lectura y escritura sobre la carpeta
+#/var/www/html      *(rw,sync,wdelay,hide,no_subtree_check,sec=sys,insecure,no_root_squash,no_all_squash) esto permite el acceso de lectura y escritura sobre la carpeta
 sudo exportfs -a #aplica los cambios
 sudo systemctl restart nfs # reinicia el nfs
--- reto3/nfs/nfs-instalar-y-montar.sh
+++ reto3/nfs/nfs-instalar-y-montar.sh
@@ -1,6 +1,6 @@
 sudo yum update -y
 sudo yum install nfs-utils -y
-sudo mkdir ~/nfsshared
+sudo mkdir -p ~/var/www/html
 sudo chown nobody:nobody ~/nfsshared
 sudo chmod 777 ~/nfsshared
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 44.216.81.68:/home/ec2-user/var/www/html  ~/var/www/html
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 54.234.20.128:/var/www/html  ~/var/www/html
