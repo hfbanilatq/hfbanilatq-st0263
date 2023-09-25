421ae758f070424f28695a2da6355c5444547458
Hector Fabio Banilat Quintero <53194376+hfbanilatq@users.noreply.github.com>
2023-09-25 08:20:03 -0500

    me rindo con lo de nfs

--- reto3/nfs/crear-directorios-nfs.sh
+++ reto3/nfs/crear-directorios-nfs.sh
@@ -1,5 +1,4 @@
 sudo mkdir -p /nfsshared
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 44.216.81.68:/nfsshared   ~/nfsshared/ #Modificar el 44.216.81.68 por la IP elastica del server nfs
 sudo emacs -nw /etc/exports #Abre el archivo usando emacs, puedes usar nano o el lector de archivos y debes agregar lo siguiente:
 #/nfsshared      *(rw,sync,wdelay,hide,no_subtree_check,sec=sys,insecure,no_root_squash,no_all_squash) esto permite el acceso de lectura y escritura sobre la carpeta
 sudo exportfs -a #aplica los cambios
--- reto3/nfs/nfs-instalar-y-montar.sh
+++ reto3/nfs/nfs-instalar-y-montar.sh
@@ -3,4 +3,4 @@ sudo yum install nfs-utils -y
 sudo mkdir ~/nfsshared
 sudo chown nobody:nobody ~/nfsshared
 sudo chmod 777 ~/nfsshared
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 44.216.81.68:/nfsshared   ~/nfsshared/
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 44.216.81.68:/home/ec2-user/var/www/html  ~/var/www/html
